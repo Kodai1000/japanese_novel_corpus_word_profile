45a8ea0c81ed908d7917cba6a2fd886a469f0363
Kodai.T <fvbcj53523@yahoo.co.jp>
2026-03-03 12:19:00 +0900

    add loading.tsx

---
+++ app/word_profile/[slug]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-xl animate-pulse">
+        読み込み中...
+      </div>
+    </div>
+  )
+}
